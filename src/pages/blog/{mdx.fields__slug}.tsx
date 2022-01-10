36ae029b62d31c74414e164cb0780b5e987ce9ae
Samuel Wong <35249407+desktopofsamuel@users.noreply.github.com>
2022-01-10 23:24:56 +0800

    feat: Add MDX and markdown posts

---
+++ src/pages/blog/{mdx.fields__slug}.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import { GatsbyImage } from 'gatsby-plugin-image';
+import { MDXRenderer } from 'gatsby-plugin-mdx';
+import Layout from '@/components/Layout';
+
+export default function BlogPostTemplate({ children, data }) {
+  const post = data.mdx;
+  return (
+    <Layout>
+      <h1>{post.frontmatter.title}</h1>
+      {post.frontmatter.featureImage && (
+        <GatsbyImage
+          image={post.frontmatter.featureImage.childImageSharp.gatsbyImageData}
+        />
+      )}
+      <MDXRenderer>{post.body}</MDXRenderer>
+    </Layout>
+  );
+}
+
+export const query = graphql`
+  query BlogPostQuery($slug: String) {
+    mdx(slug: { eq: $slug }) {
+      ...post
+    }
+  }
+`;
