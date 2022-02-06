945341ef464c6a22c43ee22c2a5ebfe0f37573c7
Samuel Wong <35249407+desktopofsamuel@users.noreply.github.com>
2022-02-06 12:09:41 +0800

    fix: build error when category is broken

--- src/templates/post-template.tsx
+++ src/templates/post-template.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import { SliceZone } from '@prismicio/react';
+import { FaExternalLinkAlt } from 'react-icons/fa';
 import { components } from '../slices';
 import Layout from '@/components/Layout';
 import SEO from '@/components/SEO';
 import GatsbyLink from '@/components/GatsbyLink';
 import { PageTitle, ResourceTitle, Subtitle } from '@/styles/TextStyles';
 import Button from '@/components/Button';
-import { FaExternalLinkAlt } from 'react-icons/fa';
 import ListPost from '@/components/ListPost';
 
 export default function PostTemplate({ data, pageContext }) {
@@ -18,18 +18,19 @@ export default function PostTemplate({ data, pageContext }) {
   return (
     <Layout>
       {/* <SEO postPath={pageContext.url} postSEO postNode={doc} /> */}
-      {doc.categories.length !== 0 && doc.categories && (
-        <GatsbyLink to={`/tags/${doc.categories[0].category.document.uid}`}>
-          <Subtitle>{doc.categories[0].category.document.data.name}</Subtitle>
-        </GatsbyLink>
-      )}
+      {doc.categories.length !== 0 &&
+        doc.categories &&
+        doc.categories[0].category.document && (
+          <GatsbyLink to={`/tags/${doc.categories[0].category.document.uid}`}>
+            <Subtitle>{doc.categories[0].category.document.data.name}</Subtitle>
+          </GatsbyLink>
+        )}
       <PageTitle>{doc.title.text}</PageTitle>
       <pre>{doc.url.url}</pre>
       <SliceZone slices={doc.body} components={components} />
       <Button to={doc.url.url}>
         Visit Now <FaExternalLinkAlt size={14} />
       </Button>
-      {console.log(related)}
       <Subtitle>Explore other related resources</Subtitle>
       <ListPost data={related} css={{ gridTemplateColumns: '1fr 1fr 1fr' }} />
     </Layout>
